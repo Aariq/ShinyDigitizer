1a0c2243db0d441bc04d1418bb3202e0dfdeeec4
Aariq <scottericr@gmail.com>
2019-08-01 11:08:47 -0400

    working on app

--- app.R
+++ app.R
@@ -1,50 +1,46 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
 library(shiny)
+library(readbitmap)
 
-# Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
+  fileInput("filename", "Choose a plot"),
+  actionButton("go", "Begin!"),
+  actionButton("cal_done", "Calibrate!"),
+  plotOutput("image", click = "plot_click", brush = "area")
 )
 
-# Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  #load the image only after clicking the button
+  img1 <- eventReactive(input$go, {
+    read.bitmap(input$filename$datapath)
+  })
+  
+  #Initialize clicks
+  click_saved <- reactiveValues(singleclick = NULL, x = NULL, y = NULL)
+  
+  #Save a single click
+  observeEvent(input$plot_click, { click_saved$singleclick <- input$plot_click })
+  
+  #Save multiple clicks
+  observeEvent(input$plot_click, {
+    click_saved$x <- c(click_saved$x, input$plot_click$x)
+    click_saved$y <- c(click_saved$y, input$plot_click$y)
+  })
+  observeEvent(input$go, {
+    output$image <- renderPlot({
+      #plot the uploaded image
+      plot.new()
+      rasterImage(img1(), 0, 0, 1, 1)
+
+      #plot a clicked point
+      points(click_saved$singleclick$x, click_saved$singleclick$y, col = "red")
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+      #continuously plot clicked points
+      points(click_saved$x, click_saved$y, col = "blue")
+      
+    }, width = dim(img1())[2], height = dim(img1())[1])
+    })
+
 }
 
-# Run the application 
 shinyApp(ui = ui, server = server)
 
